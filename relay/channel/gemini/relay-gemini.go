c2ade6565ba648427b3bdf319585404aee9d9fe6
magicdmer <magicdmer@163.com>
2025-05-14 21:26:02 +0800

    去掉默认联网选项，否则无法调用，真tm见鬼了，会提示下面错误： {   "message": "400 Search as tool is not enabled for models/gemini-2.0-flash-exp (request id: 202505142119574019754987EoQVkZh)",   "status": 400,   "error": {     "message": "Search as tool is not enabled for models/gemini-2.0-flash-exp (request id: 202505142119574019754987EoQVkZh)",     "type": "",     "param": "",     "code": 400   },   "code": 400,   "param": "",   "type": "" }

--- relay/channel/gemini/relay-gemini.go
+++ relay/channel/gemini/relay-gemini.go
@@ -64,14 +64,6 @@ func CovertGemini2OpenAI(textRequest dto.GeneralOpenAIRequest, info *relaycommon
 	}
 	geminiRequest.SafetySettings = safetySettings
 
-	if textRequest.Model == "gemini-2.0-flash-exp" {
-		geminiRequest.Tools = []GeminiChatTool{
-			{
-				GoogleSearch: make(map[string]string),
-			},
-		}
-	}
-
 	// openaiContent.FuncToToolCalls()
 	if textRequest.Tools != nil {
 		functions := make([]dto.FunctionRequest, 0, len(textRequest.Tools))
